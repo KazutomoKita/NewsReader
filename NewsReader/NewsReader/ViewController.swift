a33e2daffe60ce5ca96003b77983e7d04ba85c19
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-04-02 17:14:34 +0900

    TableView作成

--- NewsReader/NewsReader/ViewController.swift
+++ NewsReader/NewsReader/ViewController.swift
@@ -1,21 +1,18 @@
-//
-//  ViewController.swift
-//  NewsReader
-//
-//  Created by Kazutomo Kita on 2020/03/30.
-//  Copyright © 2020 Kazutomo Kita. All rights reserved.
-//
-
 import UIKit
 
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView
 
     var newsDataArray= NSArray
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         
         let requestURL = "http://appcre.net/rss.php"
         
@@ -29,14 +26,51 @@ class ViewController: UIViewController {
             //responseDataからキー値"results"を取り出す
             self.newsDataArray = responseData["results"] as! NSArray
             print("\(self.newsDataArray)")
+            //ニュース記事を取得したらテーブルビューに表示
                 
             case .Failure(let error):
                 print("通信エラー:\(error)")
             }
         }
-       
-        // Do any additional setup after loading the view.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return newsDataArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath: NSIndexPath) -> UITableViewCell {
+        
+        //StoryBoardで取得したCellを取得
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        //ニュース記事データを取得(配列の"indexPath.row版目の要素を取得")
+        let newsDic = newsDataArray[indexPath.row] as! NSDictionary
+        //タイトルとタイトルの行数、公開日時をCellにセット
+        cell.textLabel!.text = newsDic["title"] as? String
+        cell.textLabel!.numberOfLines = 3
+        cell.detailTextLabel!.text = newsDic["publishedDate"] as? String
+        
+        return cell
+    }
+    
+     //テーブルビューのセルがタップされた処理を追加
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //セルのインデックスパス番号を出力
+        print("タップされたセルのインデックスパス:\(indexPath.row)")
+        //ニュース記事データを取得
+        let newsDic = newsDataArray[indexPath.row] as! NSDictionary
+        //ニュース記事のURLを取得
+        let newsURL = newsDic["unescapedURL"] as! String
+        //StringをNSURLに変換
+        let url = NSURL(string:newsURL)
+        //UIApplicationインスタンスを作成
+        let app = UIApplication.sharedAppication()
+        //openURLメソッドURLを引数にWEBブラウザSafariを起動
+        app.openURL(url)
+    }
+    
+   
 
 
 }
