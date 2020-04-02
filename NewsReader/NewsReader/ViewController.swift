d3d27726b4fdcf722e435d788ba2870ace5b40cc
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-04-02 16:24:17 +0900

    cocoa podsにAlamofire追加

--- NewsReader/NewsReader/ViewController.swift
+++ NewsReader/NewsReader/ViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
+import Alamofire
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let requestURL = "http://appcre.net/rss.php"
+        
+        Alamofire.request(requestURL).responseJSON { response in
+            switch response.result {
+            case.Success(let json):
+                print(json)
+            case .Failure(let error):
+                print("通信エラー:\(error)")
+            }
+        }
+       
         // Do any additional setup after loading the view.
     }
 
